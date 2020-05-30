195a97cd045e9b84de2846b435f55c416d495abb
Taras Hopka <gigo123black@gmail.com>
2020-05-30 16:08:18 +0300

    create a new project

---
+++ src/main/java/ua/com/gigasoft/instrument2c/Instrument2cApplication.java
@@ -0,0 +1,13 @@
+package ua.com.gigasoft.instrument2c;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Instrument2cApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Instrument2cApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/gigasoft/instrument2c/Instrument2cApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.gigasoft.instrument2c;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Instrument2cApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
