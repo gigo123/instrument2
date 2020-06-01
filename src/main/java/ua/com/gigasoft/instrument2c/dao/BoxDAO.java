447d4f90b09fee7fddaedbf4a8f5a9e2e503ddf0
Taras Hopka <gigo123black@gmail.com>
2020-06-01 20:09:54 +0300

    changed some method to work with jpa

--- src/main/java/ua/com/gigasoft/instrument2c/dao/BoxDAO.java
+++ src/main/java/ua/com/gigasoft/instrument2c/dao/BoxDAO.java
@@ -1,12 +1,13 @@
 package ua.com.gigasoft.instrument2c.dao;
 
 import java.util.List;
+import java.util.Optional;
 
-import models.*;
+import ua.com.gigasoft.instrument2c.mainModel.Box;
 
 public interface BoxDAO {
-	public Box getBoxByID(long id);
-	public Box getBoxByNumber(int number,long idLocation);
+	public Optional<Box> getBoxByID(long id);
+	public Optional<Box> getBoxByNumber(int number,long idLocation);
 	public boolean createBox(Box box);
 	public boolean deleteBox(long id);
 	public boolean hasError();
