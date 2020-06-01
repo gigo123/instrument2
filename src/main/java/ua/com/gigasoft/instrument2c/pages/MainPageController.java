1616c9d7d2b0fdad317c4a939562bea959835186
Taras Hopka <gigo123black@gmail.com>
2020-06-01 21:18:47 +0300

    add simple index page

--- src/main/java/ua/com/gigasoft/instrument2c/pages/MainPageController.java
+++ src/main/java/ua/com/gigasoft/instrument2c/pages/MainPageController.java
@@ -1,18 +1,18 @@
 package ua.com.gigasoft.instrument2c.pages;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.servlet.ModelAndView;
 
 @Controller
-@RequestMapping("/hello")
 public class MainPageController {
-	@RequestMapping(method = RequestMethod.GET)
-	public ModelAndView returnString() {
-		ModelAndView model = new ModelAndView("WEB-INF/jsp/views/MainView");
-		model.addObject("page", "main");
-		return model;
-	}
+    @RequestMapping(value = { "/", "/index" }, method = RequestMethod.GET)
+	public String model(Model model) {
+		 
+        model.addAttribute("message", "message");
+ 
+        return "index";
+    }
 
 }
