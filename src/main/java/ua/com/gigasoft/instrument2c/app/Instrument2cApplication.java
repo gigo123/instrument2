3b38adc678e51a3718ffd0a7bd9ece5c2089ba2e
Taras Hopka <gigo123black@gmail.com>
2020-06-01 20:08:21 +0300

    add servlet interface

--- src/main/java/ua/com/gigasoft/instrument2c/app/Instrument2cApplication.java
+++ src/main/java/ua/com/gigasoft/instrument2c/app/Instrument2cApplication.java
@@ -2,9 +2,11 @@ package ua.com.gigasoft.instrument2c.app;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
 
 @SpringBootApplication
-public class Instrument2cApplication {
+public class Instrument2cApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Instrument2cApplication.class, args);
