3d047dbce08165bdc3d48f8804bec0f4e30b72de
Taras Hopka <gigo123black@gmail.com>
2020-06-01 20:09:16 +0300

    add jpa annotation

--- src/main/java/ua/com/gigasoft/instrument2c/mainModel/Box.java
+++ src/main/java/ua/com/gigasoft/instrument2c/mainModel/Box.java
@@ -1,13 +1,22 @@
 package ua.com.gigasoft.instrument2c.mainModel;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
 import javax.validation.constraints.PositiveOrZero;
 
-
+@Entity
 public class Box {
+	 @Id 
+	 @GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	@PositiveOrZero (message = "ячейка должна бить положительным числом")
+
 	private int number;
+	
 	private Location location;
+
 	private boolean notEmpty;
 	
 	
