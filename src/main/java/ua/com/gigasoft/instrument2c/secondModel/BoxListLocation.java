4f7afd43175c0c074490b662bdab7887cf6348b0
Taras Hopka <gigo123black@gmail.com>
2020-06-03 21:15:33 +0300

    object to  proces  addbox

---
+++ src/main/java/ua/com/gigasoft/instrument2c/secondModel/BoxListLocation.java
@@ -0,0 +1,51 @@
+package ua.com.gigasoft.instrument2c.secondModel;
+
+
+
+import ua.com.gigasoft.instrument2c.mainModel.Box;
+
+public class BoxListLocation extends Box{
+	private String locationWB;
+	private String startNum;
+	private String endNum;
+	private String manyBox;
+	
+	public String getManyBox() {
+		return manyBox;
+	}
+
+	public void setManyBox(String manyBox) {
+		this.manyBox = manyBox;
+	}
+
+	public String getStartNum() {
+		return startNum;
+	}
+
+	public void setStartNum(String startNum) {
+		this.startNum = startNum;
+	}
+
+	public String getEndNum() {
+		return endNum;
+	}
+
+	public void setEndNum(String endNum) {
+		this.endNum = endNum;
+	}
+
+	public String getLocationWB() {
+		return locationWB;
+	}
+
+	public void setLocationWB(String locationWB) {
+		this.locationWB = locationWB;
+	}
+
+	@Override
+	public String toString() {
+		return "BoxListLocation [locationWB=" + locationWB + "]";
+	}
+	
+
+}
