6510d8bf81e2a2eaee03f64975bc24a6dea8b2d6
gigo123 <gigo123black@gmail.com>
2020-06-05 21:09:41 +0300

    add with realtion mapping

--- src/main/java/ua/com/gigasoft/instrument2c/mainModel/Box.java
+++ src/main/java/ua/com/gigasoft/instrument2c/mainModel/Box.java
@@ -1,15 +1,14 @@
 package ua.com.gigasoft.instrument2c.mainModel;
 
 import javax.persistence.CascadeType;
+import javax.persistence.Column;
 import javax.persistence.Entity;
+import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
-import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
-import javax.persistence.PrimaryKeyJoinColumn;
 import javax.persistence.Table;
 import javax.validation.constraints.PositiveOrZero;
 
@@ -21,11 +20,11 @@ public class Box {
 	private long id;
 	@PositiveOrZero (message = "ячейка должна бить положительным числом")
 	private int number;
-	
-	@OneToOne(cascade = CascadeType.ALL)
-   @PrimaryKeyJoinColumn
+	@ManyToOne(fetch=FetchType.LAZY, 
+            cascade=CascadeType.ALL)
+    @JoinColumn (name="location")
 	private Location location;
-
+	@Column (name="notEmpty")
 	private boolean notEmpty;
 	
 	
--- src/main/java/ua/com/gigasoft/instrument2c/mainModel/Location.java
+++ src/main/java/ua/com/gigasoft/instrument2c/mainModel/Location.java
@@ -1,9 +1,13 @@
 package ua.com.gigasoft.instrument2c.mainModel;
 
+import java.util.Collection;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
@@ -17,6 +21,15 @@ public class Location {
 	private String name;
 	private boolean boxes;
 	
+	 @OneToMany (mappedBy="location",  cascade = CascadeType.ALL)
+	private Collection<Box> boxCollection;
+	
+	public Collection<Box> getBoxCollection() {
+		return boxCollection;
+	}
+	public void setBoxCollection(Collection<Box> boxCollection) {
+		this.boxCollection = boxCollection;
+	}
 	public long getId() {
 		return id;
 	}
