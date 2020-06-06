b9781e22c3ef2b1ed5b404be5465455f9d658f37
gigo123 <gigo123black@gmail.com>
2020-06-06 07:45:47 +0300

    describe getAllLocWB

--- src/main/java/ua/com/gigasoft/instrument2c/database/LocationRepository.java
+++ src/main/java/ua/com/gigasoft/instrument2c/database/LocationRepository.java
@@ -1,9 +1,15 @@
 package ua.com.gigasoft.instrument2c.database;
 
+import java.util.List;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import ua.com.gigasoft.instrument2c.mainModel.Location;
 
 public interface LocationRepository extends JpaRepository<Location,Long> {
 
+	@Query("SELECT loc FROM Location loc WHERE loc.boxes=true")
+	public List<Location> getAllLocationWB();
 }
