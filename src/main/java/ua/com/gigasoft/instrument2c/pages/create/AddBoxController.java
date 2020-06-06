70b93a627118ce84119418a6dcbbc601d3b32439
gigo123 <gigo123black@gmail.com>
2020-06-06 08:28:35 +0300

    work with add box page

--- src/main/java/ua/com/gigasoft/instrument2c/pages/create/AddBoxController.java
+++ src/main/java/ua/com/gigasoft/instrument2c/pages/create/AddBoxController.java
@@ -1,17 +1,34 @@
 package ua.com.gigasoft.instrument2c.pages.create;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
-
+import ua.com.gigasoft.instrument2c.database.LocationJPADAO;
+import ua.com.gigasoft.instrument2c.mainModel.Location;
 import ua.com.gigasoft.instrument2c.secondModel.BoxListLocation;
 
 @Controller
-
 public class AddBoxController {
+	@Autowired
+	private LocationJPADAO locDAO;
+	
 	@GetMapping("/addBox")
-	public String getAddBoxCF(BoxListLocation boxList) {
-
+	public String getAddBoxCF(BoxListLocation boxList,Model model) {
+		BoxListLocation boxlist = new BoxListLocation();
+		Map<Long, String> locationWB = new HashMap<Long, String>();
+		List<Location> locList = locDAO.getAllLocatinWB();
+		for (Location location : locList) {
+			locationWB.put(location.getId(), location.getName());
+		}
+	
+		 model.addAttribute("boxList", boxlist);
+		 model.addAttribute("locationWB", locationWB);
 		return "addBox";
 	}
 
