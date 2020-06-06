60ed4613842ac9ab4fba562f175e6a768edb1542
gigo123 <gigo123black@gmail.com>
2020-06-06 15:49:01 +0300

    fix some error

--- src/main/java/ua/com/gigasoft/instrument2c/database/InstrumentRepository.java
+++ src/main/java/ua/com/gigasoft/instrument2c/database/InstrumentRepository.java
@@ -10,6 +10,6 @@ import org.springframework.stereotype.Repository;
 import ua.com.gigasoft.instrument2c.mainModel.Instrument;
 @Repository
 public interface InstrumentRepository extends JpaRepository<Instrument,Long> {
-	@Query("SELECT inst FROM Instrument inst WHERE inst.name=:locname")
+	@Query("SELECT inst FROM Instrument inst WHERE inst.name=:instname")
 	public Optional<Instrument> getInstrumentByName(@Param("instname")String name);
 }
