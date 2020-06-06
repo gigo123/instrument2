7ede94a14978f760ea97e7392356c0ac940694e8
gigo123 <gigo123black@gmail.com>
2020-06-06 07:47:29 +0300

    add fill of location list

--- src/main/java/ua/com/gigasoft/instrument2c/secondModel/BoxListLocation.java
+++ src/main/java/ua/com/gigasoft/instrument2c/secondModel/BoxListLocation.java
@@ -2,15 +2,25 @@ package ua.com.gigasoft.instrument2c.secondModel;
 
 
 
+import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+import ua.com.gigasoft.instrument2c.dao.LocationDAO;
 import ua.com.gigasoft.instrument2c.mainModel.Box;
+import ua.com.gigasoft.instrument2c.mainModel.Location;
 
 public class BoxListLocation extends Box{
 	private Map<Long, String> locationWB;
 	private String startNum;
 	private String endNum;
 	private String manyBox;
+	@Autowired
+	private LocationDAO locDAO;
 	
 	public String getManyBox() {
 		return manyBox;
@@ -49,6 +59,15 @@ public class BoxListLocation extends Box{
 	@Override
 	public String toString() {
 		return "BoxListLocation [locationWB=" + locationWB + "]";
+	}
+
+	public BoxListLocation() {
+
+		Map<Long, String> locationWB = new HashMap<Long, String>();
+		List<Location> locList = locDAO.getAllLocatinWB();
+		for (Location location : locList) {
+			locationWB.put(location.getId(), location.getName());
+		}
 	}
 	
 
