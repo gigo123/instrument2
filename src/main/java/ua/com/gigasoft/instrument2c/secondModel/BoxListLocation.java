283c7b6f8eb370ba4522cb13f25f02e5cb3dcb5b
gigo123 <gigo123black@gmail.com>
2020-06-06 08:28:18 +0300

    remove wrong field

--- src/main/java/ua/com/gigasoft/instrument2c/secondModel/BoxListLocation.java
+++ src/main/java/ua/com/gigasoft/instrument2c/secondModel/BoxListLocation.java
@@ -2,25 +2,13 @@ package ua.com.gigasoft.instrument2c.secondModel;
 
 
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.ClassPathXmlApplicationContext;
-
-import ua.com.gigasoft.instrument2c.dao.LocationDAO;
 import ua.com.gigasoft.instrument2c.mainModel.Box;
-import ua.com.gigasoft.instrument2c.mainModel.Location;
 
 public class BoxListLocation extends Box{
-	private Map<Long, String> locationWB;
 	private String startNum;
 	private String endNum;
 	private String manyBox;
-	@Autowired
-	private LocationDAO locDAO;
+
 	
 	public String getManyBox() {
 		return manyBox;
@@ -48,26 +36,12 @@ public class BoxListLocation extends Box{
 
 	
 
-	public Map<Long, String> getLocationWB() {
-		return locationWB;
-	}
+	
 
-	public void setLocationWB(Map<Long, String> locationWB) {
-		this.locationWB = locationWB;
-	}
 
-	@Override
-	public String toString() {
-		return "BoxListLocation [locationWB=" + locationWB + "]";
-	}
 
 	public BoxListLocation() {
 
-		Map<Long, String> locationWB = new HashMap<Long, String>();
-		List<Location> locList = locDAO.getAllLocatinWB();
-		for (Location location : locList) {
-			locationWB.put(location.getId(), location.getName());
-		}
 	}
 	
 
