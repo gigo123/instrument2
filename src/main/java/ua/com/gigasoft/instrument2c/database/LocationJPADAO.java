b9495e50ca3ec4e13213dec96fbb61adf824594a
gigo123 <gigo123black@gmail.com>
2020-06-06 08:28:05 +0300

    add missing annotation

--- src/main/java/ua/com/gigasoft/instrument2c/database/LocationJPADAO.java
+++ src/main/java/ua/com/gigasoft/instrument2c/database/LocationJPADAO.java
@@ -4,10 +4,12 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import ua.com.gigasoft.instrument2c.dao.LocationDAO;
 import ua.com.gigasoft.instrument2c.mainModel.Location;
 
+@Service
 public class LocationJPADAO implements LocationDAO {
 
 	@Autowired
--- src/main/java/ua/com/gigasoft/instrument2c/database/LocationRepository.java
+++ src/main/java/ua/com/gigasoft/instrument2c/database/LocationRepository.java
@@ -5,9 +5,10 @@ import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
 import ua.com.gigasoft.instrument2c.mainModel.Location;
-
+@Repository
 public interface LocationRepository extends JpaRepository<Location,Long> {
 
 	@Query("SELECT loc FROM Location loc WHERE loc.boxes=true")
