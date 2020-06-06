b177c62f2f5e3ef6daf8d68c37bb7f7a18473bce
gigo123 <gigo123black@gmail.com>
2020-06-06 08:43:22 +0300

    add again locationWb as string

--- src/main/java/ua/com/gigasoft/instrument2c/secondModel/BoxListLocation.java
+++ src/main/java/ua/com/gigasoft/instrument2c/secondModel/BoxListLocation.java
@@ -8,10 +8,19 @@ public class BoxListLocation extends Box{
 	private String startNum;
 	private String endNum;
 	private String manyBox;
-
+	private String locationWB;
+	
 	
 	public String getManyBox() {
 		return manyBox;
+	}
+
+	@Override
+	public String toString() {
+		return "BoxListLocation [" + (startNum != null ? "startNum=" + startNum + ", " : "")
+				+ (endNum != null ? "endNum=" + endNum + ", " : "")
+				+ (manyBox != null ? "manyBox=" + manyBox + ", " : "")
+				+ (locationWB != null ? "locationWB=" + locationWB : "") + "]";
 	}
 
 	public void setManyBox(String manyBox) {
@@ -20,6 +29,14 @@ public class BoxListLocation extends Box{
 
 	public String getStartNum() {
 		return startNum;
+	}
+
+	public String getLocationWB() {
+		return locationWB;
+	}
+
+	public void setLocationWB(String locationWB) {
+		this.locationWB = locationWB;
 	}
 
 	public void setStartNum(String startNum) {
