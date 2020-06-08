78bbcc7ad9aae2af74f9cb130a6a733cdb04e792
gigo123 <gigo123black@gmail.com>
2020-06-08 14:52:45 +0300

    add ajax searchBy id class

---
+++ src/main/java/ua/com/gigasoft/instrument2c/support/SearchById.java
@@ -0,0 +1,15 @@
+package ua.com.gigasoft.instrument2c.support;
+
+
+public class SearchById {
+	private int boxId;
+
+	public int getBoxId() {
+		return boxId;
+	}
+
+	public void setBoxId(int boxId) {
+		this.boxId = boxId;
+	}
+	
+}
