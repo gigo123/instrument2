232fe75e4fe4bd88f4b8a06f7e5ecb62d1fcfab0
gigo123 <gigo123black@gmail.com>
2020-06-08 14:54:44 +0300

    work with create in doc page

--- src/main/java/ua/com/gigasoft/instrument2c/pages/create/CreateInDocCont.java
+++ src/main/java/ua/com/gigasoft/instrument2c/pages/create/CreateInDocCont.java
@@ -1,6 +1,5 @@
 package ua.com.gigasoft.instrument2c.pages.create;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -10,35 +9,30 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
 
+import ua.com.gigasoft.instrument2c.dao.InstrumentDAO;
 import ua.com.gigasoft.instrument2c.database.LocationJPADAO;
+import ua.com.gigasoft.instrument2c.mainModel.Instrument;
 import ua.com.gigasoft.instrument2c.mainModel.Location;
-import ua.com.gigasoft.instrument2c.secondModel.BoxListLocation;
-import ua.com.gigasoft.instrument2c.secondModel.ExDocWEB;
 import ua.com.gigasoft.instrument2c.secondModel.ExDocWEBList;
 
 @Controller
 public class CreateInDocCont {
-	
-@Autowired
+
+	@Autowired
 	ExDocWEBList exDocWEBList;
-	
+
 	@Autowired
 	private LocationJPADAO locDAO;
-	
+	@Autowired
+	private InstrumentDAO instDAO;
+
 	@GetMapping("/createInDoc")
 	public String getInDocCF(Model model) {
-
-	//ExDocWEB doc = new ExDocWEB();
-	//	 exDocWEBList = new ExDocWEBList();
-	//	List<ExDocWEB> docList = new ArrayList<ExDocWEB>();
-	//	docList.add(doc);
-//	exDocWEBList.setDocList(docList);
 		model.addAttribute("docList", exDocWEBList);
 		return "createInDoc";
 	}
+
 	@ModelAttribute("locationList")
 	public Map<Long, String> getLocationWB() {
 		Map<Long, String> locationWB = new HashMap<Long, String>();
@@ -49,4 +43,14 @@ public class CreateInDocCont {
 		return locationWB;
 	}
 
+	@ModelAttribute("instrumentList")
+	private Map<Long, String> getInstrumentMap() {
+		Map<Long, String> instrumentMap = new HashMap<Long, String>();
+
+		List<Instrument> instList = instDAO.getAllInstrument();
+		for (Instrument instrument : instList) {
+			instrumentMap.put(instrument.getId(), instrument.getName());
+		}
+		return instrumentMap;
+	}
 }
