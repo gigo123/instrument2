d005b84a7e3d83a5431109cd5f7aa11859cc2e84
Taras <gigo123black@gmail.com>
2020-08-03 12:32:57 +0300

    rename validator classes

---
+++ src/main/java/ua/com/gigasoft/instrument2c/customValidator/AddBoxNumerValidator.java
@@ -0,0 +1,18 @@
+package ua.com.gigasoft.instrument2c.customValidator;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import javax.validation.Constraint;
+import javax.validation.Payload;
+
+@Target ({ElementType.TYPE , ElementType.FIELD})
+@Retention (RetentionPolicy.RUNTIME)
+@Constraint (validatedBy = AddBoxNumberVC.class)
+public @interface AddBoxNumerValidator {
+	   String message() default "номер занят";
+	    Class<?>[] groups() default {};
+	    Class<? extends Payload>[] payload() default {};
+}
