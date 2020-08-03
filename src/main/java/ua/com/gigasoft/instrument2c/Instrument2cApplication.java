f057e7d884ce3b204c0525772c4ac90a96d800da
Taras <gigo123black@gmail.com>
2020-08-03 12:33:59 +0300

    inject messageSource

--- src/main/java/ua/com/gigasoft/instrument2c/Instrument2cApplication.java
+++ src/main/java/ua/com/gigasoft/instrument2c/Instrument2cApplication.java
@@ -1,13 +1,17 @@
 package ua.com.gigasoft.instrument2c;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.context.MessageSource;
 
 
 @SpringBootApplication
 public class Instrument2cApplication extends SpringBootServletInitializer {
 
+	 @Autowired
+	    private MessageSource messageSource;
 	public static void main(String[] args) {
 		SpringApplication.run(Instrument2cApplication.class, args);
 	}
