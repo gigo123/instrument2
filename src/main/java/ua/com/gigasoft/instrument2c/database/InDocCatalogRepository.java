ca7867d00a57477322b565adb825921bbf51acee
Taras <gigo123black@gmail.com>
2020-08-03 12:33:23 +0300

    add repository annotation

--- src/main/java/ua/com/gigasoft/instrument2c/database/InDocCatalogRepository.java
+++ src/main/java/ua/com/gigasoft/instrument2c/database/InDocCatalogRepository.java
@@ -1,10 +1,10 @@
 package ua.com.gigasoft.instrument2c.database;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import org.springframework.stereotype.Repository;
 
 import ua.com.gigasoft.instrument2c.mainModel.InDocCatalog;
-
+@Repository
 public interface InDocCatalogRepository extends JpaRepository<InDocCatalog,Long> {
 
 }
