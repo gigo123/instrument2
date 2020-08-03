d3f7587d55a5141b98c7d6e9550273d382cc0039
Taras <gigo123black@gmail.com>
2020-08-03 10:23:08 +0300

    take method from previus project

--- src/main/java/ua/com/gigasoft/instrument2c/support/DocCreateWorker.java
+++ src/main/java/ua/com/gigasoft/instrument2c/support/DocCreateWorker.java
@@ -1,12 +1,15 @@
 package ua.com.gigasoft.instrument2c.support;
 
 import java.time.LocalDate;
+import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
 import ua.com.gigasoft.instrument2c.mainModel.Location;
+import ua.com.gigasoft.instrument2c.mainModel.OutDoc;
 import ua.com.gigasoft.instrument2c.secondModel.ExDocWEB;
+import ua.com.gigasoft.instrument2c.secondModel.ExDocWEBList;
 import ua.com.gigasoft.instrument2c.mainModel.Box;
 import ua.com.gigasoft.instrument2c.mainModel.DocModel;
 import ua.com.gigasoft.instrument2c.mainModel.ExDoc;
@@ -19,6 +22,7 @@ import ua.com.gigasoft.instrument2c.dao.InstrumentDAO;
 import ua.com.gigasoft.instrument2c.dao.LocationDAO;
 import ua.com.gigasoft.instrument2c.dao.StorageDAO;
 import ua.com.gigasoft.instrument2c.mainModel.ExDocCatalog;
+import ua.com.gigasoft.instrument2c.mainModel.InDoc;
 import ua.com.gigasoft.instrument2c.secondModel.DocType;
 import ua.com.gigasoft.instrument2c.secondModel.ExDocTempStore;
 
@@ -29,6 +33,152 @@ public class DocCreateWorker {
 	BoxDAO boxDAO;
 	InstrumentDAO instDAO;
 	LocationDAO locDAO;
+
+	
+	public  String createExDocUnwrap(ExDocWEBList docListWrap, DocType docType) {
+		List<ExDocWEB> docList = docListWrap.getDocList();
+		StringBuilder errorText = new StringBuilder("<ul>");
+		List<ExDocTempStore> docTempList = new ArrayList<ExDocTempStore>();
+		for (int i = 0; i < docList.size(); i++) {
+			ExDocTempStore tempDoc = makeExDoc(docList.get(i), i, docType);
+			errorText.append(tempDoc.getErrorString());
+			docTempList.add(makeExDoc(docList.get(i), i, docType));
+		}
+		errorText.append("</ul>");
+		String errString = errorText.toString();
+		if (errString.equals("<ul></ul>")) {
+			String error = writeDocCatolog(docType,docTempList.size(),calcTotalAmount(docTempList));
+			if (!error.equals("<li>РѕС€С‹Р±РєР° Р±Р°Р·Рё РґР°РЅРЅРёС… </li>")) {
+				long catalogId = 0;
+					catalogId = docCatDAO.getDocCatalogBySnumber(error).getId();
+				for (ExDocTempStore exDocTempStore : docTempList) {
+					error += writeExDoc(exDocTempStore.getDoc(), catalogId, exDocTempStore.getOutStorageId(), docType);
+				}
+			}
+			
+			return error;
+		} else {
+			return errString;
+		}
+
+	}
+
+	private ExDocTempStore makeExDoc(ExDocWEB docW, int number, DocType docType) {
+		String errorText = "";
+		number++;
+		DocModel doc = null;
+		ExDocTempStore tempDoc = null;
+		if (docType == DocType.EXDOC) {
+			doc = new ExDoc();
+			tempDoc = checkInParam(docW, number, (ExDoc) doc);
+			errorText = tempDoc.getErrorString();
+			tempDoc = checkOutParam(docW, number, tempDoc.getDoc());
+			errorText += tempDoc.getErrorString();
+		}
+		if (docType == DocType.INDOC) {
+			doc = new InDoc();
+			tempDoc = checkOutParam(docW, number, doc);
+			errorText += tempDoc.getErrorString();
+		}
+		if (docType == DocType.OUTDOC) {
+			doc = new OutDoc();
+			tempDoc = checkOutParam(docW, number, doc);
+			errorText += tempDoc.getErrorString();
+		}
+		if (errorText.equals("")) {
+			tempDoc = checkInstrument(docW, number, tempDoc.getDoc(), docType);
+			errorText += tempDoc.getErrorString();
+			if (errorText.equals("")) {
+				tempDoc.getDoc().setAmount(docW.getAmount());
+				errorText = tempDoc.getErrorString();
+				if (docType == DocType.EXDOC) {
+					errorText += checkBox((ExDoc) tempDoc.getDoc());
+				}
+				tempDoc.setErrorString(errorText);
+				return tempDoc;
+			} else {
+				return new ExDocTempStore(errorText, doc, 0);
+			}
+		} else {
+			return new ExDocTempStore(errorText, doc, 0);
+		}
+	}
+
+	private  String checkBox(ExDoc doc) {
+		long inID = doc.getInBox().getId();
+		long outId = doc.getOutBox().getId();
+		if (inID == outId) {
+			return "<li>РѕРґРёРЅР°РєРѕРІРёРµ  СЏС‡РµР№РєРё РїСЂРёРµРјР° Рё РІРёРґР°С‡Рё</li>";
+		}
+		return "";
+
+	}
+	private  ExDocTempStore checkInstrument(ExDocWEB docW, int number, DocModel doc, DocType docType) {
+		StringBuilder errorText = new StringBuilder("");
+		Box box = doc.getOutBox();
+		long storageId = 0;
+		Optional<Instrument> instrumentOpt = instDAO.getInstrumentByid(Long.parseLong(docW.getInstrument()));
+		if (instrumentOpt.isPresent()) {
+			Instrument instrument = instrumentOpt.get();
+			if (docType == DocType.EXDOC || docType == DocType.OUTDOC) {
+				List<Storage> storeList = storageDAO.getStorageByBox(box.getId());
+				boolean hasInstrument = false;
+				for (int i = 0; i < storeList.size(); i++) {
+					Instrument tempInst = storeList.get(i).getInstrument();
+					if (tempInst != null) {
+						if (tempInst.getId() == instrument.getId()) {
+							hasInstrument = true;
+							storageId = storeList.get(i).getId();
+						}
+					}
+				}
+				if (hasInstrument) {
+					Storage storage = storageDAO.getStorageByID(storageId);
+					float instLeft = storage.getAmount() - docW.getAmount();
+					if (instLeft > 0.0) {
+						doc.setInstrument(instrument);
+					} else {
+						errorText.append("<li>РЅРµРґРѕСЃС‚Р°С‡РЅРѕ РёРЅСЃС‚СЂСѓРјРµРЅС‚Р° РґР»СЏ РІРёРґР°С‡Рё  РІ СЃС‚СЂРѕРєРµ " + number + "</li>");
+					}
+
+				} else {
+					errorText.append("<li>РЅРµС‚ РёРЅСЃС‚СЂСѓРјРµРЅС‚Р° РІ СЏС‡РµРєРµ РІРёРґР°С‡Рё  РІ СЃС‚СЂРѕРєРµ " + number + "</li>");
+				}
+
+			}
+			if (docType == DocType.INDOC) {
+				doc.setInstrument(instrument);
+			}
+			
+		} else {
+			errorText.append("<li>РЅРµ РїСЂР°РІРёР»СЊРЅРёР№ РёРЅСЃС‚СЂСѓРјРµРЅС‚ РІ СЃС‚СЂРѕРєРµ " + number + " </li>");
+		}
+		ExDocTempStore tempDoc = new ExDocTempStore(errorText.toString(), doc, storageId);
+		return tempDoc;
+	}
+	
+	private  ExDocTempStore checkInParam(ExDocWEB docW, int number, ExDoc doc) {
+		String errorText = "";
+		try {
+			Optional<Location>  location = locDAO.getLocById(Long.parseLong(docW.getOutLocation()));
+			if (location.isPresent()) {
+				doc.setOutLocation(location.get());
+				Optional<Box> box = boxDAO.getBoxByID(docW.getInBox());
+				if (box.isPresent()) {
+					doc.setInBox(box.get());
+				} else {
+					errorText = "<li>РЅРµРїСЂР°РІРёР»СЊРЅР°СЏ РїСЂРёРЅРёРјР°СЋС‰Р°СЏ СЏС‡РµР№РєР° РІ СЃС‚СЂРѕРєРµ " + number + "</li>";
+				}
+			} else {
+				errorText = "<li>РЅРµРїСЂР°РІРёР»СЊРЅРѕРµ РјРµСЃС‚Рѕ РїСЂРёРµРјР° РІ СЃС‚РѕРєРµ " + number + "</li>";
+			}
+		} catch (Exception e) {
+			errorText = "<li>РЅРµРїСЂР°РІРёР»СЊРЅРѕРµ РјРµСЃС‚Рѕ РїСЂРёРµРјР° РІ СЃС‚РѕРєРµ " + number + "</li>";
+		}
+
+		ExDocTempStore tempDoc = new ExDocTempStore(errorText, doc, 0);
+		return tempDoc;
+	}
 
 	private  ExDocTempStore checkOutParam(ExDocWEB docW, int number, DocModel doc) {
 		String errorText = "";
