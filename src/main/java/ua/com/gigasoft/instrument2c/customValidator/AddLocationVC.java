b0cbd227a58453a0c674956051b43b628e0f49d3
Taras <gigo123black@gmail.com>
2020-08-13 15:18:48 +0300

    Update AddLocationVC.java
    some bug, disable validation

--- src/main/java/ua/com/gigasoft/instrument2c/customValidator/AddLocationVC.java
+++ src/main/java/ua/com/gigasoft/instrument2c/customValidator/AddLocationVC.java
@@ -9,26 +9,27 @@ import org.springframework.beans.BeanWrapperImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 
 import ua.com.gigasoft.instrument2c.database.LocationJPADAO;
+import ua.com.gigasoft.instrument2c.database.LocationRepository;
 import ua.com.gigasoft.instrument2c.mainModel.Location;
 
 public class AddLocationVC implements ConstraintValidator<AddLocationValidator, Object>{
 
 	@Autowired
-	private LocationJPADAO locDAO;
-
+	//private LocationJPADAO locDAO;
+    private LocationRepository locationRepo;
 	public void initialize(AddLocationValidator constraint) {
 	}
 	@Override
 	public boolean isValid(Object value, ConstraintValidatorContext context) {
 		String name = (String) new BeanWrapperImpl(value).getPropertyValue("name");
 		context.disableDefaultConstraintViolation();
-		Optional<Location> loc = locDAO.getLocByName(name);
-		if (loc.isPresent()) {
-			context.buildConstraintViolationWithTemplate("место храненя существует")
-			.addPropertyNode("name").addConstraintViolation();
-					return false;
-		}
-		
+		//Optional<Location> loc = locDAO.getLocByName(name);
+		//Optional<Location> loc = locationRepo.getLocByName(name);
+	//	if (loc.isPresent()) {
+		//	context.buildConstraintViolationWithTemplate("место храненя существует")
+		//	.addPropertyNode("name").addConstraintViolation();
+		//			return false;
+		//}
 		return true;
 	}
 
