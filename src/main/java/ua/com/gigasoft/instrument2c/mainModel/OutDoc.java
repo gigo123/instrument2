6fa05f2e5b342f3c5a5845b7397816ffe176a0d1
Taras <gigo123black@gmail.com>
2020-08-20 09:32:04 +0300

    remove separete doc classes
    leave one doc class

--- src/main/java/ua/com/gigasoft/instrument2c/mainModel/OutDoc.java
+++
@@ -1,9 +0,0 @@
-package ua.com.gigasoft.instrument2c.mainModel;
-
-
-public class OutDoc extends DocModel {
-	public OutDoc(Location outLocation, Box outBox, DocCatalog catalogId, Instrument instrument, float amount) {
-		super(outLocation, outBox, catalogId, instrument, amount);
-	}
-	public OutDoc() {};
-}
